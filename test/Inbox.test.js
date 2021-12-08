ed4762b04e80c3a11faa8c33ec01b4d6547bcebe
SagarPEPCODING <raaasamrahs1234@gmail.com>
2021-12-09 01:38:05 +0530

    08-12-2021 making a contract inbox and learn some testing stuff today, deployed that contract as well, learn about mocha testing framework, learn about ganache/TestRPC library which helps to test on local network, used mocha functions like it, describe, beforeEach, used Web3 service for deploy the contract

---
+++ test/Inbox.test.js
@@ -0,0 +1,65 @@
+// contract test code will go here
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
+
+// mocha is testing framework, which is used to test frontend code, backend code or ethereum code as well so it is a general purpose testing framework
+
+// ************* 1st Testing **************
+// class Car {
+//   park() {
+//     return "stopped";
+//   }
+//   drive() {
+//     return "vroom";
+//   }
+// }
+
+// let car;
+// beforeEach(() => {
+//   car = new Car();
+// });
+
+// describe("Car", () => {
+//   it("can park", () => {
+//     assert.equal(car.park(), "stopped");
+//   });
+//   it("can run", () => {
+//     assert.equal(car.drive(), "vroom");
+//   });
+// });
+
+// Video-49 **************
+// beforeEach(() => {
+//   web3.eth.getAccounts().then((fetchedAccounts) => {
+//     console.log(fetchedAccounts);
+//   });
+// });
+
+// describe("Inbox", () => {
+//   it("deploys a contract", () => {});
+// });
+// *********************
+
+// video-50 **********
+let accounts;
+let inbox;
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  //use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(accounts);
+    console.log(inbox);
+  });
+});
